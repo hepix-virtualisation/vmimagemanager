4439c0bbb629e143b1d99cbd88dc752927b6711b
Owen M Synge <owen.synge@desy.de>
2008-02-19 01:20:43 +0000

    configuration and a new version including more documentation and xfs support.

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from distutils.core import setup
 setup(name='vmimagemanager',
-      version='0.0.3',
+      version='0.0.4',
       description="Virualisation image mover",
       author="owen synge",
       author_email="owen.synge@desy.de",
       url="http://www.python.org/sigs/distutils-sig/",
       package_dir={'': '.'},     
-      data_files=[('/etc/vmimagemanager/',['config/vmimagemanager-xen-example.template',
-        'config/vmimagemanager-example-d430.cfg',
+      data_files=[('/etc/vmimagemanager/',[ 'config/vmimagemanager-example-d430.cfg',
 	   'config/vmimagemanager-example-au.cfg',   'config/vmimagemanager-example-irl.cfg',
 	    'config/vmimagemanager-example-esys.cfg']),
 	   ('/usr/share/doc/vmimagemanager/', ['README']),
-      ],
+       ('/var/lib/vmimagemanager/etc/', ['config/vmimagemanager-xen-example.template'])],
       scripts=['vmimagemanager.py']
       )
