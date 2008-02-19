213a58f0429be76bec03444ed69ae4f5b631a82d
Owen M Synge <owen.synge@desy.de>
2008-02-19 22:16:53 +0000

    easier configuration and more documentation

--- vmimagemanager/setup.py
+++ vmimagemanager/setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
-setup(name='vmimagemanager',
-      version='0.0.4',
-      description="Virualisation image mover",
-      author="owen synge",
-      author_email="owen.synge@desy.de",
-      url="http://www.python.org/sigs/distutils-sig/",
-      package_dir={'': '.'},     
-      data_files=[('/etc/vmimagemanager/',[ 'config/vmimagemanager-example-d430.cfg',
-	   'config/vmimagemanager-example-au.cfg',   'config/vmimagemanager-example-irl.cfg',
-	    'config/vmimagemanager-example-esys.cfg']),
-	   ('/usr/share/doc/vmimagemanager/', ['README']),
-       ('/var/lib/vmimagemanager/etc/', ['config/vmimagemanager-xen-example.template'])],
-      scripts=['vmimagemanager.py']
-      )
+ProjectName='vmimagemanager'
+versionString='0.0.4.1'
+docRoot='/usr/share/doc/' + ProjectName + '-' + versionString
+setup(name=ProjectName,
+        version=versionString,
+        description="Virualisation manager, xen template generator and image mover.",
+        author="owen synge",
+        author_email="owen.synge@desy.de",
+        url="http://www.python.org/sigs/distutils-sig/",
+        package_dir={'': '.'},     
+        data_files=[('/etc/' + ProjectName , ['vmimagemanager.cfg','config/xen.template']),
+        (docRoot + '/example-cfg/',[ 'config/vmimagemanager-example-d430.cfg',
+        'config/vmimagemanager-example-au.cfg',   'config/vmimagemanager-example-irl.cfg',
+        'config/vmimagemanager-example-esys.cfg','config/vmimagemanager-example-whitehouse.cfg']),
+        (docRoot, ['README']),
+        (docRoot + '/example-template/', ['config/vmimagemanager-xen-example.template']),
+        ],
+    scripts=['vmimagemanager.py']
+)
