9fe30493d1f043aa984d83c5128c23de658c3c77
Owen M Synge <owen.synge@desy.de>
2008-02-20 00:19:05 +0000

    some moving about and some fixes

--- vmimagemanager/setup.py
+++ vmimagemanager/setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 ProjectName='vmimagemanager'
-versionString='0.0.4.1'
+versionString='0.0.5'
 docRoot='/usr/share/doc/' + ProjectName + '-' + versionString
 setup(name=ProjectName,
         version=versionString,
