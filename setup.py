19434d449e5381cb73874d6985cb41719698a463
Owen M Synge <owen.synge@desy.de>
2009-07-10 07:28:17 +0000

    setup.py got lost some how

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+ProjectName='vmimagemanager'
+versionString='0.1.0'
+docRoot='/usr/share/doc/' + ProjectName + '-' + versionString
+setup(name=ProjectName,
+        version=versionString,
+        description="Virualisation manager, xen template generator and image mover.",
+        author="owen synge",
+        author_email="owen.synge@desy.de",
+        url="http://www.python.org/sigs/distutils-sig/",
+        package_dir={'': '.'},     
+        data_files=[('/etc/' + ProjectName , ['vmimagemanager.cfg','config/xen.template']),
+        (docRoot + '/example-cfg/',[ 'config/vmimagemanager-example-d430.cfg',
+        'config/vmimagemanager-example-au.cfg',   'config/vmimagemanager-example-irl.cfg',
+        'config/vmimagemanager-example-esys.cfg','config/vmimagemanager-example-whitehouse.cfg']),
+        (docRoot, ['README']),
+        (docRoot + '/example-template/', ['config/vmimagemanager-xen-example.template',
+	  'config/xen.template.example.full.virtualisation']),
+        ],
+    scripts=['vmimagemanager.py']
+)
