1fcd7f51b555eb5cc0a3d0842d27564b9389a289
Owen Synge <owen.synge@desy.de>
2012-09-03 12:52:11 +0200

    Module lxml  is not available in SL 5.X or RHEL 5.X

--- vmimagemanager/vmim/cxsl.py
+++ vmimagemanager/vmim/cxsl.py
@@ -5,8 +5,26 @@ import logging, logging.config
 
 
 from os import path
-from lxml import etree
-
+try: from lxml import etree
+except ImportError:
+  try:
+    # Python 2.5
+    import xml.etree.cElementTree as etree
+  except ImportError:
+    try:
+      # Python 2.5
+      import xml.etree.ElementTree as etree
+    except ImportError:
+      try:
+        # normal cElementTree install
+        import cElementTree as etree
+      except ImportError:
+        try:
+          # normal ElementTree install
+          import elementtree.ElementTree as etree
+        except ImportError:
+          print("Failed to import ElementTree from any known place")
+          
 try: from StringIO import StringIO
 except ImportError:
     from io import BytesIO
