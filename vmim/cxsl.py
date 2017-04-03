2eecb3c60b5332a72654477cbec237e0bdf170be
Owen Synge <owen.synge@jaysnest.de>
2017-04-03 21:38:54 +0200

    E999 TabError: inconsistent use of tabs and spaces in indentation

--- vmim/cxsl.py
+++ vmim/cxsl.py
@@ -52,7 +52,7 @@ class virtualhostKvm(cvirthost.virtualhost):
 
     def genXmlShouldExist(self):
     
-    	defaultTransfrom = '''<xsl:stylesheet  version="1.0"
+        defaultTransfrom = '''<xsl:stylesheet  version="1.0"
 xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 <xsl:template match="/">
     <foo><xsl:value-of select="/domain/name" /></foo>
