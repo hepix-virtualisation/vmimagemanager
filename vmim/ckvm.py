1c9bce212436c1602f3e89a86742f9baffefa110
Owen Synge <owen.synge@jaysnest.de>
2017-04-03 21:13:15 +0200

    assigned to but never used

--- vmim/ckvm.py
+++ vmim/ckvm.py
@@ -25,11 +25,10 @@ class virtualhostKvm(cvirthost.virtualhost):
         os_type = SubElement(os, "type",arch="x86_64",machine="pc")
         os_type.text = str("hvm")
         features = SubElement(domain, "features")
-        acpi = SubElement(features, "acpi")
-        apic = SubElement(features, "apic")
-        pae = SubElement(features, "pae")
-        
-        clock = SubElement(domain, "clock",offset="utc")
+        SubElement(features, "acpi")
+        SubElement(features, "apic")
+        SubElement(features, "pae")
+        SubElement(domain, "clock",offset="utc")
         on_power_off  = SubElement(domain, "on_poweroff")
         on_power_off.text = "destroy"
         on_reboot = SubElement(domain, "on_reboot")
@@ -47,17 +46,14 @@ class virtualhostKvm(cvirthost.virtualhost):
              log.debug("bridge=%s" % (self.Bridge))
              interface = SubElement(devices, "interface")
              interface.set('type', "bridge")
-             mac_address = SubElement(interface, "mac",address='%s' % (self.HostMacAddress))
-             source = SubElement(interface, "source",bridge='%s' % (self.Bridge))
-             target = SubElement(interface, "target",dev='vnet1')
+             SubElement(interface, "mac",address='%s' % (self.HostMacAddress))
+             SubElement(interface, "source",bridge='%s' % (self.Bridge))
+             SubElement(interface, "target",dev='vnet1')
         else:
             self.logger.debug("Has no mac address")
         serial = SubElement(devices, "serial")
         serial.set('type', "pty")
-        serial_target  = SubElement(serial, "target",port="0")
-        
-        #print "self.genXml=" + self.genXml()
-        #print "genXmlShouldExist=" + text
+        SubElement(serial, "target",port="0")
         text = tostring(domain)
         self.logger.debug(text)
         return text
