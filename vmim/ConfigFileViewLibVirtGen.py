006d08cf48914f6383c5adbab0afed38c1bad6c9
Owen Synge <owen.synge@jaysnest.de>
2017-04-03 20:46:09 +0200

    vmim/ConfigFileViewLibVirtGen.py:82:34: E999 TabError: inconsistent use of tabs and spaces in indentation

--- vmim/ConfigFileViewLibVirtGen.py
+++ vmim/ConfigFileViewLibVirtGen.py
@@ -72,7 +72,7 @@ class virtualhostKvm:
     
     def genXmlShouldExist(self, BoxName):
     
-    	defaultTransfrom = '''<xsl:stylesheet  version="1.0"
+        defaultTransfrom = '''<xsl:stylesheet  version="1.0"
 xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 <xsl:template match="/">
     <foo><xsl:value-of select="/domain/name" /></foo>
