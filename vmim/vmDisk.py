133fcbd4ef979cfedfdd75e4546f5fd881210e28
Owen Synge <owen.synge@jaysnest.de>
2017-04-03 21:51:50 +0200

    E999 TabError: inconsistent use of tabs and spaces in indentation

--- vmim/vmDisk.py
+++ vmim/vmDisk.py
@@ -37,7 +37,7 @@ class diskFacade(object):
             elif name == "lvm":
                 self._uploaderImp = diskMounterLvm()
             else:
-	    	if hasattr(self, '_uploaderImp'):
+                if hasattr(self, '_uploaderImp'):
                     del(self._uploaderImp)
             if hasattr(self, '_uploaderImp'):
                 self._uploaderImp.path = self.path
